4bad9ea0015f897d854cc6bc6434baefc54a91a3
darshanigupta <darshanigupta2004@gmail.com>
2025-03-24 19:27:57 +0530

    Update task_2.cpp

--- task_2.cpp
+++ task_2.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    double a, b;
-    char c;
-    cin >> a >> c >> b;
-    if (c == '+') cout << a + b;
-    else if (c == '-') cout << a - b;
-    else if (c == '*') cout << a * b;
-    else if (c == '/') {
-        if (b == 0) cout << "Error";
-        else cout << a / b;
-    } else cout << "Invalid";
+    char op;
+    double num1, num2;
+    
+    while (true) {
+        cout << "\nSimple Calculator\n";
+        cout << "Choose operation (+, -, *, /) or 'q' to quit: ";
+        cin >> op;
+
+        if (op == 'q' || op == 'Q') {
+            cout << "Exiting Calculator. Thank you!\n";
+            break;
+        }
+
+        cout << "Enter first number: ";
+        cin >> num1;
+        cout << "Enter second number: ";
+        cin >> num2;
+
+        switch (op) {
+            case '+': cout << "Result: " << num1 + num2 << endl; break;
+            case '-': cout << "Result: " << num1 - num2 << endl; break;
+            case '*': cout << "Result: " << num1 * num2 << endl; break;
+            case '/': 
+                if (num2 != 0) cout << "Result: " << num1 / num2 << endl;
+                else cout << "Error! Division by zero is not allowed.\n";
+                break;
+            default: cout << "Invalid operation! Please enter a valid operator.\n";
+        }
+    }
+
     return 0;
 }
